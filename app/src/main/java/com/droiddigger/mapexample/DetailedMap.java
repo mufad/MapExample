a162714c112f3bdcf8f94234486b1835687f945b
Monwar Jahan <mmufad@gmail.com>
2017-02-05 17:02:38 +0600

    Buttons added

--- app/src/main/java/com/droiddigger/mapexample/DetailedMap.java
+++ app/src/main/java/com/droiddigger/mapexample/DetailedMap.java
@@ -11,15 +11,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
-public class DetailedMap extends AppCompatActivity implements OnMapReadyCallback{
+public class DetailedMap extends AppCompatActivity implements OnMapReadyCallback {
 
-    boolean mapReady=false;
+    boolean mapReady = false;
     GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_map);
-        MapFragment mapFragment= (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -42,10 +43,10 @@ public class DetailedMap extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mapReady=true;
-        this.googleMap=googleMap;
-        LatLng dhaka=new LatLng(23.7, 90.3);
-        CameraPosition target=CameraPosition.builder().target(dhaka).zoom(14).build();
+        mapReady = true;
+        this.googleMap = googleMap;
+        LatLng dhaka = new LatLng(23.7, 90.3);
+        CameraPosition target = CameraPosition.builder().target(dhaka).zoom(14).build();
         this.googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 }
